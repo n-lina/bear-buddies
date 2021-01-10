2d90cd31b2a0f8d5e4497d985ce90986ce4d9e48
chntlc <chantaalchung@gmail.com>
2021-01-09 20:18:49 -0800

    animate bear

--- frontend/src/animations/bear.js
+++ frontend/src/animations/bear.js
@@ -2,6 +2,7 @@ import React, { useRef, useState, useEffect } from 'react';
 import { Illustration, Ellipse, Shape, RoundedRect, useRender, Polygon } from 'react-zdog';
 // New react-spring target, for native animation outside of React
 import { a, useSpring } from 'react-spring/zdog';
+import { observer } from "mobx-react"
 
 /** --- Basic, re-usable shapes -------------------------- */
 const TAU = Math.PI * 2
@@ -19,11 +20,50 @@ const Arm = props => (
     </a.Shape>
 )
 
+function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    // remember latest callback
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    // setup the interval
+    useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        }
+
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
+}
+
 /** --- Assembly ---------------------------------------- */
-function Test() {
+function Bear(props) {
+    const { templateStore } = props;
+
     // useRender allows us to hook into the render-loop
     const [up, setUp] = useState(true)
-    const { rotation, color, color_tint, size } = useSpring({ size: up ? 1.2 : 0.2, color: up ? '#7a5843' : 'tomato', color_tint: up ? '#7b6044' : 'tomato', rotation: up ? 0 : Math.PI })
+
+    useInterval(() => {
+        console.log('hi' + up + templateStore.healthBool);
+        setUp(!up)
+    }, 450);
+
+    const leftArm = templateStore.healthBool ? 3 : 6;
+    const leftLeg = templateStore.healthBool ? 3 : 0;
+    const rightArm = templateStore.healthBool ? -6 : -3;
+    const rightLeg = templateStore.healthBool ? -3 : 0;
+
+    // const { rotation, color, color_tint, size } = useSpring({ size: up ? 1.2 : 0.2, color: up ? '#7a5843' : '#7a5843', color_tint: up ? '#7b6044' : '#7b6044', rotation: up ? 0 : Math.PI });
+    const size = 1.2;
+    const color = "#7a5843"
+    const color_tint = "#7b6044"
+    const rotation = 0;
+    const { la, ll, ra, rl } = useSpring({ la: up ? 3 : 6, ll: up ? 0 : 3, ra: up ? -3 : -6, rl: up ? 0 : -3 });
     const ref = useRef()
     let t = 0
     useRender(() => (ref.current.rotate.y = Math.cos((t += 0.1) / TAU)));
@@ -42,21 +82,13 @@ function Test() {
             <Ellipse diameter={1} translate={{ x: -3.5, y: 1.5, z: 6.5 }} rotate={{ z: TAU / 4 }} closed color="indianred" stroke={0.5} fill /> {/*left blush*/}
             <Ellipse diameter={1} translate={{ x: 3.5, y: 1.5, z: 6.5 }} rotate={{ z: TAU / 4 }} closed color="indianred" stroke={0.5} fill /> {/*left right*/}
             <a.Ellipse height={7} width={4} ref={ref} stroke={14} translate={{ x: 0, y: 13 }} color={color} fill /> {/*body*/}
-            <a.Ellipse height={6} width={3} ref={ref} stroke={5} translate={{ x: 11, y: 10 }} rotate={{ z: TAU / 3 }} color={color_tint} fill /> {/*left arm*/}
-            <a.Ellipse height={6} width={3} ref={ref} stroke={5} translate={{ x: -11, y: 10 }} rotate={{ z: TAU / -3 }} color={color_tint} fill /> {/*right arm*/}
-            <a.Ellipse height={4} width={4} ref={ref} stroke={7} translate={{ x: 7, y: 23 }} color={color_tint} fill /> {/*left leg*/}
-            <a.Ellipse height={4} width={4} ref={ref} stroke={7} translate={{ x: -7, y: 23 }} color={color_tint} fill /> {/*right leg*/}
+            <a.Ellipse height={6} width={1.5} ref={ref} stroke={5} translate={{ x: 11, y: 10 }} rotate={{ z: TAU / (up ? 6 : leftArm) }} color={color_tint} fill /> {/*left arm*/}
+            <a.Ellipse height={6} width={1.5} ref={ref} stroke={5} translate={{ x: -11, y: 10 }} rotate={{ z: TAU / ra }} color={color_tint} fill /> {/*right arm*/}
+            <a.Ellipse height={4} width={2} ref={ref} stroke={7} translate={{ x: 6, y: 23 }} rotate={{ z: TAU / ll }} color={color_tint} fill /> {/*left leg*/}
+            <a.Ellipse height={4} width={2} ref={ref} stroke={7} translate={{ x: -6, y: 23 }} rotate={{ z: TAU / rl }} color={color_tint} fill /> {/*right leg*/}
         </a.Shape>
-
-
     )
 }
 
 
-function Bear() {
-    return (
-        <Test />
-    )
-}
-
-export default Bear;
+export default observer(Bear);
