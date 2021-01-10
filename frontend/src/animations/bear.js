08a66b7ce5fb268dfc9b4de15126a491be11468b
Yiyuan Sun <50027648+nusnauyiy@users.noreply.github.com>
2021-01-09 21:49:02 -0800

    sleeping bear modeling 2

--- frontend/src/animations/bear.js
+++ frontend/src/animations/bear.js
@@ -150,7 +150,88 @@ function BatheBear() {
 }
 
 function SleepBear() {
+    const TAU = Math.PI * 2
 
+    const [up, setUp] = useState(true)
+
+    useInterval(() => {
+        setUp(!up)
+    }, 450);
+
+    const leftArm = up ? 3 : 6;
+    const leftLeg = up ? 1 : 3;
+    const rightArm = up ? -3 : -6;
+    const rightLeg = up ? 1 : -3;
+
+    // const { rotation, color, color_tint, size } = useSpring({ size: up ? 1.2 : 0.2, color: up ? '#7a5843' : '#7a5843', color_tint: up ? '#7b6044' : '#7b6044', rotation: up ? 0 : Math.PI });
+    const size = 1.2;
+    const color = "#7a5843"
+    const color_tint = "#7b6044"
+    const rotation = 0;
+    // const { rotation, color, size } = useSpring({ size: up ? 1.2 : bbb, color: up ? '#EA0' : aaa, rotation: up ? 0 : ccc })
+    const { la, ll, ra, rl } = useSpring({ la: up ? 3 : leftArm, ll: up ? 0 : leftLeg, ra: up ? -3 : rightArm, rl: up ? 0 : rightLeg });
+
+    const ref = useRef()
+    let t = 0
+    useRender(() => (ref.current.rotate.y = Math.cos((t += 0.1) / TAU)));
+    // const Eye = props => <Ellipse diameter={1.5} quarters={2} translate={{ x: -2.2, y: 0, z: 6.8 }} rotate={{ z: -TAU / 4 }} color="#241d19" stroke={0.5} />
+    // const Leg = props => (
+    //     <a.Shape path={[{ y: 0 }, { y: 6 }]} translate={{ x: -3 }} color="#747B9E" stroke={4} {...props}>
+    //         <Shape path={[{ y: 0 }, { y: 6 }]} translate={{ y: 6 }} rotate={{ x: -TAU / 8 }} color="#747B9E" stroke={4} />
+    //         <RoundedRect width={2} height={4} cornerRadius={1} translate={{ y: 12, z: -3.5 }} rotate={{ x: TAU / 6 }} color="#444B6E" fill stroke={4} />
+    //     </a.Shape>
+    // )
+    // const Arm = props => (
+    //     <a.Shape path={[{ y: 0 }, { y: 4 }]} translate={{ x: -5, y: -2 }} color="#F0F2EF" stroke={4} {...props}>
+    //         <Shape path={[{ y: 0 }, { y: 4 }]} translate={{ y: 6 }} rotate={{ x: TAU / 8 }} color="#EA0" stroke={4} />
+    //         <Shape translate={{ z: 4, y: 9, x: 0 }} stroke={5.4} color="#EA0" />
+    //     </a.Shape>
+    // )
+        return (
+            <a.Shape stroke={0} translate={{ y: 10}}>
+                <a.Shape stroke={15} translate={{ y: 0, x: 10 }} rotate={{z: TAU/4, x: TAU/4}} color={color}> {/*head*/}
+                    <a.Shape stroke={4} translate={{ x: -7, y: -4 }} color={color_tint} fill /> {/*left ear*/}
+                    <a.Shape stroke={4} translate={{ x: 7, y: -4 }} color={color_tint} fill /> {/*right ear*/}
+                    {/*<Eye /> left eye*/}
+                    <a.Ellipse stroke={0.3} diameter={3} quarters={1} translate={{ x: -2.2, y: -3, z: 5 }} rotate={{ z: TAU / 3 }} color="#241d19"  /> {/*left eye*/}
+                    {/*<Eye translate={{ x: 2.2, z: 6.8 }} /> right eye*/}
+                    <a.Ellipse stroke={0.3} diameter={3} quarters={1} translate={{ x: 2.2, y: -3, z: 5 }} rotate={{ z: TAU / 3 }} color="#241d19"  /> {/*right eye*/}
+                    <a.Shape height={3} width={2} scale={size} translate={{ y: 2.5, z: 6.8 }} rotate={{ z: TAU / 4 }} closed color="#b08b74" stroke={6} fill /> {/*mouth*/}
+                    <a.Shape height={1.3} width={1} scale={size} translate={{ y: 2, z: 9 }} rotate={{ z: TAU / 4 }} closed color="#241d19" stroke={3} fill> {/*nose*/}
+                        <a.Ellipse height={.1} width={.05} translate={{ x: -.5, y: 0, z: 0 }} color="white" fill />
+                    </a.Shape>
+                    <a.Ellipse height={7} width={4} stroke={14} translate={{ x: 0, y: 13 }} color={color} fill /> {/*body*/}
+                    <a.Ellipse height={6} width={3} stroke={5} translate={{ x: 11, y: 10 }} rotate={{ z: TAU / 3 }} color={color_tint} fill /> {/*left arm*/}
+                    <a.Ellipse height={6} width={3} stroke={5} translate={{ x: -11, y: 10 }} rotate={{ z: TAU / -3 }} color={color_tint} fill /> {/*right arm*/}
+                    <a.Ellipse height={4} width={4} stroke={7} translate={{ x: 7, y: 23 }} color={color_tint} fill /> {/*left leg*/}
+                    <a.Ellipse height={4} width={4} stroke={7} translate={{ x: -7, y: 23 }} color={color_tint} fill /> {/*right leg*/}
+
+                </a.Shape>
+                <a.Shape  stroke={0} ref={ref}>
+                    <a.Shape closed={false} stroke={1} color='#636' path={[{ x: -1, y: -2 }, // start at top left
+                        { x:  1, y: -2 }, // line to top right
+                        { x: -1, y:  2 }, // line to bottom left
+                        { x:  1, y:  2 }]}
+                             translate={{x: -20, y: 1.5, z: 15}}
+                             rotate={{ y: TAU / 3 }}
+                    />
+                    <a.Shape closed={false} stroke={1.6} color='#636' path={[{ x: -1, y: -3 }, // start at top left
+                        { x:  1, y: -3 }, // line to top right
+                        { x: -1, y:  3 }, // line to bottom left
+                        { x:  1, y:  3 }]}
+                             translate={{x: 20, y: 1.5, z: 20}}
+                    />
+                    <a.Shape closed={false} stroke={2} color='#636' path={[{ x: -1, y: -3 }, // start at top left
+                        { x:  1, y: -3 }, // line to top right
+                        { x: -1, y:  3 }, // line to bottom left
+                        { x:  1, y:  3 }]}
+                             translate={{x: 0, y: -15, z: -30}}
+                             rotate={{ y: TAU }}
+                    />
+                </a.Shape>
+
+            </a.Shape>
+        )
 }
 
 function PetBear() {
@@ -167,15 +248,16 @@ function Bear(props) {
     if (templateStore.healthBool) {
         bear = PlayBear();
         return bear;
-    } if (templateStore.eatBool) {
+    } else if (templateStore.eatBool) {
 
-    } if (templateStore.sleepBool) {
+    } else if (templateStore.sleepBool) {
+        bear = SleepBear();
+        return bear;
+    } else if (templateStore.cleanBool) {
 
-    } if (templateStore.cleanBool) {
+    } else if (templateStore.breatheBool) {
 
-    } if (templateStore.breatheBool) {
-
-    } if (templateStore.petBool) {
+    } else if (templateStore.petBool) {
 
     } else {
         bear = NormalBear();
