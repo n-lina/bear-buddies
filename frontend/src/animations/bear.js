50262d33ba0c2140df7365528839563d90f8a377
chntlc <chantaalchung@gmail.com>
2021-01-09 21:11:11 -0800

    delegate bear actions

--- frontend/src/animations/bear.js
+++ frontend/src/animations/bear.js
@@ -41,21 +41,17 @@ function useInterval(callback, delay) {
     }, [delay]);
 }
 
-/** --- Assembly ---------------------------------------- */
-function Bear(props) {
-    const { templateStore } = props;
-
-    // useRender allows us to hook into the render-loop
+function NormalBear() {
     const [up, setUp] = useState(true)
 
     useInterval(() => {
         setUp(!up)
     }, 450);
 
-    const leftArm = templateStore.healthBool ? 3 : 6;
-    const leftLeg = templateStore.healthBool ? 0 : 3;
-    const rightArm = templateStore.healthBool ? -3 : -6;
-    const rightLeg = templateStore.healthBool ? 0 : -3;
+    const leftArm = up ? 3 : 6;
+    const leftLeg = up ? 1 : 3;
+    const rightArm = up ? -3 : -6;
+    const rightLeg = up ? 1 : -3;
 
     // const { rotation, color, color_tint, size } = useSpring({ size: up ? 1.2 : 0.2, color: up ? '#7a5843' : '#7a5843', color_tint: up ? '#7b6044' : '#7b6044', rotation: up ? 0 : Math.PI });
     const size = 1.2;
@@ -63,11 +59,12 @@ function Bear(props) {
     const color_tint = "#7b6044"
     const rotation = 0;
     // const { rotation, color, size } = useSpring({ size: up ? 1.2 : bbb, color: up ? '#EA0' : aaa, rotation: up ? 0 : ccc })
-    const { la, ll, ra, rl } = useSpring({ la: up ? 3 : leftArm, ll: up ? 0 : leftLeg, ra: up ? -3 : rightArm, rl: up ? 0 : rightLeg});
+    const { la, ll, ra, rl } = useSpring({ la: up ? 3 : leftArm, ll: up ? 0 : leftLeg, ra: up ? -3 : rightArm, rl: up ? 0 : rightLeg });
 
     const ref = useRef()
     let t = 0
     useRender(() => (ref.current.rotate.y = Math.cos((t += 0.1) / TAU)));
+
     return (
         <a.Shape ref={ref} stroke={15} translate={{ y: -9.5 }} color={color}> {/*head*/}
             <a.Shape ref={ref} stroke={4} translate={{ x: -7, y: -4 }} color={color_tint} fill /> {/*left ear*/}
@@ -83,13 +80,107 @@ function Bear(props) {
             <Ellipse diameter={1} translate={{ x: -3.5, y: 1.5, z: 6.5 }} rotate={{ z: TAU / 4 }} closed color="indianred" stroke={0.5} fill /> {/*left blush*/}
             <Ellipse diameter={1} translate={{ x: 3.5, y: 1.5, z: 6.5 }} rotate={{ z: TAU / 4 }} closed color="indianred" stroke={0.5} fill /> {/*left right*/}
             <a.Ellipse height={7} width={4} ref={ref} stroke={14} translate={{ x: 0, y: 13 }} color={color} fill /> {/*body*/}
-            <a.Ellipse height={6} width={1.5} ref={ref} stroke={5} translate={{ x: 11, y: 10 }} rotate={{ z: TAU / (up ? 3 : leftArm)}} color={color_tint} fill /> {/*left arm*/}
-            <a.Ellipse height={6} width={1.5} ref={ref} stroke={5} translate={{ x: -11, y: 10 }} rotate={{ z: TAU / (up ? -3 : rightArm) }} color={color_tint} fill /> {/*right arm*/}
-            <a.Ellipse height={4} width={2} ref={ref} stroke={7} translate={{ x: 6, y: 23 }} rotate={{ z: TAU / (up ? 0.001 : leftLeg) }} color={color_tint} fill /> {/*left leg*/}
-            <a.Ellipse height={4} width={2} ref={ref} stroke={7} translate={{ x: -6, y: 23 }} rotate={{ z: TAU / (up ? 0.001 : rightLeg) }} color={color_tint} fill /> {/*right leg*/}
-        {/* had to put 0.001 cuz cant divide by 0    */}
+            <a.Ellipse height={6} width={1.5} ref={ref} stroke={5} translate={{ x: 11, y: 10 }} rotate={{ z: TAU / 3 }} color={color_tint} fill /> {/*left arm*/}
+            <a.Ellipse height={6} width={1.5} ref={ref} stroke={5} translate={{ x: -11, y: 10 }} rotate={{ z: TAU / -3 }} color={color_tint} fill /> {/*right arm*/}
+            <a.Ellipse height={4} width={2} ref={ref} stroke={7} translate={{ x: 6, y: 23 }} rotate={{ z: TAU / 1 }} color={color_tint} fill /> {/*left leg*/}
+            <a.Ellipse height={4} width={2} ref={ref} stroke={7} translate={{ x: -6, y: 23 }} rotate={{ z: TAU / 1 }} color={color_tint} fill /> {/*right leg*/}
+            {/* had to put 0.001 cuz cant divide by 0    */}
         </a.Shape>
-    )
+    );
+}
+
+function FeedBear() {
+
+}
+
+function PlayBear() {
+    const [up, setUp] = useState(true)
+
+    useInterval(() => {
+        setUp(!up)
+    }, 450);
+
+    const leftArm = up ? 3 : 6;
+    const leftLeg = up ? 1 : 3;
+    const rightArm = up ? -3 : -6;
+    const rightLeg = up ? 1 : -3;
+
+    // const { rotation, color, color_tint, size } = useSpring({ size: up ? 1.2 : 0.2, color: up ? '#7a5843' : '#7a5843', color_tint: up ? '#7b6044' : '#7b6044', rotation: up ? 0 : Math.PI });
+    const size = 1.2;
+    const color = "#7a5843"
+    const color_tint = "#7b6044"
+    const rotation = 0;
+    // const { rotation, color, size } = useSpring({ size: up ? 1.2 : bbb, color: up ? '#EA0' : aaa, rotation: up ? 0 : ccc })
+    const { la, ll, ra, rl } = useSpring({ la: up ? 3 : leftArm, ll: up ? 0 : leftLeg, ra: up ? -3 : rightArm, rl: up ? 0 : rightLeg });
+
+    const ref = useRef()
+    let t = 0
+    useRender(() => (ref.current.rotate.y = Math.cos((t += 0.1) / TAU)));
+
+    return (
+        <a.Shape ref={ref} stroke={15} translate={{ y: -9.5 }} color={color}> {/*head*/}
+            <a.Shape ref={ref} stroke={4} translate={{ x: -7, y: -4 }} color={color_tint} fill /> {/*left ear*/}
+            <a.Shape ref={ref} stroke={4} translate={{ x: 7, y: -4 }} color={color_tint} fill /> {/*right ear*/}
+            {/*<Eye /> left eye*/}
+            <a.Shape ref={ref} stroke={1.2} translate={{ x: -2.2, y: -1, z: 6.8 }} color="#241d19" fill /> {/*left eye*/}
+            {/*<Eye translate={{ x: 2.2, z: 6.8 }} /> right eye*/}
+            <a.Shape ref={ref} stroke={1.2} translate={{ x: 2.2, y: -1, z: 6.8 }} color="#241d19" fill /> {/*right eye*/}
+            <a.Shape height={3} width={2} ref={ref} scale={size} stroke={10} translate={{ y: 2.5, z: 6.8 }} rotate={{ z: TAU / 4 }} closed color="#b08b74" stroke={6} fill /> {/*mouth*/}
+            <a.Shape height={1.3} width={1} ref={ref} scale={size} stroke={4} translate={{ y: 2, z: 9 }} rotate={{ z: TAU / 4 }} closed color="#241d19" stroke={3} fill> {/*nose*/}
+                <a.Ellipse height={.1} width={.05} ref={ref} translate={{ x: -.5, y: 0, z: 0 }} color="white" fill />
+            </a.Shape>
+            <Ellipse diameter={1} translate={{ x: -3.5, y: 1.5, z: 6.5 }} rotate={{ z: TAU / 4 }} closed color="indianred" stroke={0.5} fill /> {/*left blush*/}
+            <Ellipse diameter={1} translate={{ x: 3.5, y: 1.5, z: 6.5 }} rotate={{ z: TAU / 4 }} closed color="indianred" stroke={0.5} fill /> {/*left right*/}
+            <a.Ellipse height={7} width={4} ref={ref} stroke={14} translate={{ x: 0, y: 13 }} color={color} fill /> {/*body*/}
+            <a.Ellipse height={6} width={1.5} ref={ref} stroke={5} translate={{ x: 11, y: 10 }} rotate={{ z: TAU / (up ? 3 : leftArm) }} color={color_tint} fill /> {/*left arm*/}
+            <a.Ellipse height={6} width={1.5} ref={ref} stroke={5} translate={{ x: -11, y: 10 }} rotate={{ z: TAU / (up ? -3 : rightArm) }} color={color_tint} fill /> {/*right arm*/}
+            <a.Ellipse height={4} width={2} ref={ref} stroke={7} translate={{ x: 6, y: 23 }} rotate={{ z: TAU / (up ? 1 : leftLeg) }} color={color_tint} fill /> {/*left leg*/}
+            <a.Ellipse height={4} width={2} ref={ref} stroke={7} translate={{ x: -6, y: 23 }} rotate={{ z: TAU / (up ? 1 : rightLeg) }} color={color_tint} fill /> {/*right leg*/}
+            {/* had to put 0.001 cuz cant divide by 0    */}
+        </a.Shape>
+    );
+}
+
+function BreatherBear() {
+
+}
+
+function BatheBear() {
+
+}
+
+function SleepBear() {
+
+}
+
+function PetBear() {
+
+}
+
+
+
+/** --- Delegate types of bear based on boolean ---------------------------------------- */
+function Bear(props) {
+    const { templateStore } = props;
+
+    let bear;
+    if (templateStore.healthBool) {
+        bear = PlayBear();
+        return bear;
+    } if (templateStore.eatBool) {
+
+    } if (templateStore.sleepBool) {
+
+    } if (templateStore.cleanBool) {
+
+    } if (templateStore.breatheBool) {
+
+    } if (templateStore.petBool) {
+
+    } else {
+        bear = NormalBear();
+        return bear;
+    }
 }
 
 
