fc83d3c40e5375a906168b217954d9ba8c8eb02c
unknown <linanguyen@hotmail.com>
2021-01-09 20:20:21 -0800

    bug fix

--- frontend/src/models/Template.js
+++ frontend/src/models/Template.js
@@ -27,44 +27,50 @@ const Template = types
   }))
   .actions(self => ({
     handleEat(val){
+      if (self.full >= 100) return
       self.eatBool = val
       if (val) {
-        self.full += 10
+        self.full = Math.max(100, self.full + 10)
         self.checkLevelUp()
       }
     }, 
     handleClean(val){
+      if (self.clean >= 100) return
       self.cleanBool = val
       if (val) {
-        self.clean += 10
+        self.clean = Math.max(100, self.clean +10)
         self.checkLevelUp()
       }
     }, 
     handlePlay(val){
+      if (self.health >= 100) return
       self.playBool = val
       if (val) {
-        self.health += 10
+        self.health = Math.max(100, self.health +10)
         self.checkLevelUp()
       }
     }, 
     handleSleep(val){
+      if (self.energy >= 100) return
       self.sleepBool = val
       if (val) {
-        self.energy += 10
+        self.energy = Math.max(100, self.energy + 10)
         self.checkLevelUp()
       }
     }, 
     handleBreathe(val){
+      if (self.calm >= 100) return
       self.calmBool = val
       if (val) {
-        self.calm += 10
+        self.calm = Math.max(100, self.calm + 10)
         self.checkLevelUp()
       }
     }, 
     handlePet(val){
+      if (self.happy >= 100) return
       self.happyBool = val
       if (val) {
-        self.happy += 10
+        self.happy = Math.max(100, self.happy + 10)
         self.checkLevelUp()
       }
     }, 
