e17a41300d34d5124132037eb2cfa3ad60fb65dd
Christopher Hui <chui@coursera.org>
2021-01-09 19:50:57 -0800

    put outside to try deploying

---
+++ admin.js
@@ -0,0 +1,44 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const getAuthToken = (req, res, next) => {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(' ')[0] === 'Bearer'
+  ) {
+    req.authToken = req.headers.authorization.split(' ')[1];
+  } else {
+    req.authToken = null;
+  }
+  next();
+};
+
+
+const checkIfAuthenticated = (req, res, next) => {
+  getAuthToken(req, res, async () => {
+    try {
+      const {
+        authToken
+      } = req;
+      const userInfo = await admin
+        .auth()
+        .verifyIdToken(authToken);
+      req.authId = userInfo.uid;
+      return next();
+    } catch (e) {
+      return res
+        .status(401)
+        .send({
+          error: 'You are not authorized to make this request'
+        });
+    }
+  });
+};
+
+module.exports.admin = admin;
+module.exports.checkIfAuthenticated = checkIfAuthenticated;
